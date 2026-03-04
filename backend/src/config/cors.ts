1d50d2482bf7230666fb548cfec0b2a9842dc56a
Ashithdeveloper <ashithashith593@gmail.com>
2026-03-04 20:22:54 +0530

    update server level

---
+++ backend/src/config/cors.ts
@@ -0,0 +1,28 @@
+import { CronJob } from "cron";
+import https from "https";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const job = new CronJob("*/14 * * * *", function () {
+  const url = process.env.API_URL;
+
+  if (!url) {
+    console.error("API_URL is not defined in env");
+    return;
+  }
+
+  https
+    .get(url, (res) => {
+      if (res.statusCode === 200) {
+        console.log("GET request sent successfully");
+      } else {
+        console.log("GET request failed", res.statusCode);
+      }
+    })
+    .on("error", (err) => {
+      console.error("Error while sending request", err);
+    });
+});
+
+export default job;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,15 +4,16 @@ import dotenv from "dotenv";
 import connectDB from "./db/DataBase";
 import userRoute from "./routes/userlogin.route";
 import webDataRoute from "./routes/data.route";
+import job from "./config/cors";
+
 
 dotenv.config();
 
 const app = express();
-console.log("CLOUD NAME:", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("API KEY:", process.env.CLOUDINARY_API_KEY);
-console.log("API SECRET:", process.env.CLOUDINARY_API_SECRET);
+
 app.use(express.json());
 app.use(cors());
+job.start();
 
 app.use("/api", userRoute);
 app.use("/api", webDataRoute);
